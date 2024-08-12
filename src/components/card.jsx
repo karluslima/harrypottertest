0e46ee04e03ff9677193d0ff660a718480b1f4a3
karluslima <karlus@live.com>
2024-08-12 16:18:40 -0300

    ajuestes finais

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,11 +1,10 @@
 import { useHarryCharacters } from "../hooks/useHarryCharacters"
 import "../assets/card.scss"
-import "../assets/no-photo.png"
+import nophoto from "../assets/no-photo.png"
 
 
 export default function Card() {
   const {data} = useHarryCharacters();
-  const nophoto = "https://e7.pngegg.com/pngimages/536/147/png-clipart-logo-harry-potter-and-the-cursed-child-hogwarts-james-potter-fictional-universe-of-harry-potter-harry-potter-logo-magic.png"
   return (
   <div className="characters">
     {data?.map(card => {
@@ -14,7 +13,7 @@ export default function Card() {
           <div className="character--photo">
             <div className="character--photo--gradient"></div>
             <div className="character--name"> {card.name} </div>
-            <img src={card?.image ? card.image : '../assets/no-photo.png' } alt={card.name} />
+            <img src={card?.image ? card.image : nophoto } alt={card.name} />
           </div>
           <div className="character--birth"> <b>Data de Nascimento: </b> {card.dateOfBirth ? card.dateOfBirth : 'sem informção '} </div>
           <div className="character--house"> <b>Casa:</b> {card?.house ? card.house : 'casa desconhecida'} </div>
